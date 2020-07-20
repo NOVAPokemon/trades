9d3ae59a19f6855351fe98b3849ad19888cb0334
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-20 12:30:57 +0100

    fix flag check

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 		utils.SetLogFile(serverName)
 	}
 
-	if *flags.DelayedComms {
+	if !*flags.DelayedComms {
 		commsManager = utils.CreateDefaultCommunicationManager()
 	} else {
 		locationTag := utils.GetLocationTag(utils.DefaultLocationTagsFilename, serverName)
